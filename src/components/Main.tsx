70f1f0f6e50f49d0e17aaf4bb60ba1e77e6d20bf
salihayazdir <salihayazdir@gmail.com>
2024-02-10 14:19:59 +0300

    dev

---
+++ src/components/Main.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+
+type Props = {};
+
+export default function Main({}: Props) {
+  const [wordsInput, setWordsInput] = useState<string>('');
+  const [textInput, setTextInput] = useState<string>('');
+
+  const words = wordsInput.split(' ');
+  const wordsNotFound = words.filter((word) => !textInput.includes(word));
+
+  return (
+    <div className='grid grid-cols-3 grid-rows-2 gap-6 w-full'>
+      <div className='flex flex-col gap-2'>
+        <h2 className='text-xl font-bold'>Words</h2>
+        <textarea
+          name='words'
+          value={wordsInput}
+          onChange={(e) => setWordsInput(e.target.value)}
+          className='bg-white/10 p-4 rounded-lg w-full h-full'
+        />
+      </div>
+      <div className='flex flex-col gap-2 row-span-2 col-span-2'>
+        <h2 className='text-xl font-bold'>Text</h2>
+        <textarea
+          name='words'
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+          className='bg-white/10 p-4 rounded-lg h-full'
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <h2 className='text-xl font-bold text-white/40'>Words Not Found</h2>
+        <div className='bg-white/10 p-4 rounded-lg w-full h-full'>
+          {wordsNotFound.join(' ')}
+        </div>
+      </div>
+    </div>
+  );
+}
